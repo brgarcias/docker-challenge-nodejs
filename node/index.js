d6701317139f3826c66a3e8c4bf1072628449af6
bruno.garcia <bruno.silva@trocafone.com>
2023-10-24 00:08:52 -0300

    feat: implementing project

---
+++ node/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+const port = 8080;
+
+const config = {
+    host: "db",
+    user: "root",
+    password: "bruno123",
+    database: "nodedb",
+};
+const connection = mysql.createConnection(config);
+
+const insertName = (name) => {
+    return new Promise((resolve, reject) => {
+        const sqlInsert = `INSERT INTO people (name) VALUES (?)`;
+        connection.query(sqlInsert, [name], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+const getNames = () => {
+    return new Promise((resolve, reject) => {
+        const sqlSelect = `SELECT name FROM people`;
+        connection.query(sqlSelect, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+app.get("/", async (req, res) => {
+    try {
+        await insertName("Bruno");
+
+        const results = await getNames();
+
+        const nameList = results.map((row) => row.name);
+
+        const message = `
+            <h1>Full Cycle Rocks!</h1>
+            <p>- Lista de nomes cadastrados no banco de dados.</p>
+            <ul>
+                ${nameList.map((name) => `<li>${name}</li>`).join("")}
+            </ul>
+         `;
+
+        res.send(message);
+    } catch (error) {
+        res.status(500).send("Server error.");
+    }
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
